6ead5e9f6b4eb6b684bce28b50dbd77bc11a1069
sunzu chris <sunzuchris250@gmail.com>
2025-01-19 00:33:12 +0200

    Get is back

--- app/api/accounts/[id]/route.ts
+++ app/api/accounts/[id]/route.ts
@@ -6,7 +6,27 @@ import { NextRequest, NextResponse } from "next/server";
 
 
 
+ export async function GET(req: Request,{ params }: { params: Record<string, string> }) {
+    try {
+        const id = parseInt(params.id, 10);
 
+        if(isNaN(id)) {
+            return NextResponse.json({ error: 'Invalid ID format' }, { status: 400 });
+        }
+
+       const data = await db.select().from(accounts).where(eq(accounts.id, id));
+
+        if(!data.length) {
+            return NextResponse.json({ error: 'No Account found'}, { status: 404 });
+        }
+
+        return NextResponse.json(data, { status: 200 });
+            
+    } catch (error) {
+        console.error('Error fetching Account:', error); 
+        return NextResponse.json({ error: "Failed to fetch Account"}, { status: 404 });
+    }
+}
 
  export async function PATCH(req: NextRequest, { params }: { params: { id: string } }) {
     try {
@@ -25,35 +45,34 @@ import { NextRequest, NextResponse } from "next/server";
     }
   }
 
-  // export async function DELETE(req: Request, { params }: { params: { id: string } }) {
-  //     try {
-  //       const {id} = await params;
-  //       const parseID = parseInt(id);
+  export async function DELETE(req: Request, { params }: { params: { id: string } }) {
+      try {
+        const id = parseInt(params.id);
     
-  //       if (!id) {
-  //         return NextResponse.json(
-  //           { message: "Account ID is required for deletion." },
-  //           { status: 400 }
-  //         );
-  //       }
+        if (!id) {
+          return NextResponse.json(
+            { message: "Account ID is required for deletion." },
+            { status: 400 }
+          );
+        }
     
-  //       const deletedAccount = await deleteAccount(Number(parseID));
+        const deletedAccount = await deleteAccount(Number(id));
     
-  //       if (!deletedAccount) {
-  //         return NextResponse.json(
-  //           { message: "Account not found." },
-  //           { status: 404 }
-  //         );
-  //       }
+        if (!deletedAccount) {
+          return NextResponse.json(
+            { message: "Account not found." },
+            { status: 404 }
+          );
+        }
     
-  //       return NextResponse.json(
-  //         { message: "Account deleted successfully" },
-  //         { status: 200 }
-  //       );
-  //     } catch (error) {
-  //       return NextResponse.json(
-  //         { message: "Error deleting Account", error },
-  //         { status: 500 }
-  //       );
-  //     }
-  //   }
+        return NextResponse.json(
+          { message: "Account deleted successfully" },
+          { status: 200 }
+        );
+      } catch (error) {
+        return NextResponse.json(
+          { message: "Error deleting Account", error },
+          { status: 500 }
+        );
+      }
+    }
