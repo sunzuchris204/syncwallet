c0ed16cea363e4ff6c84dc11928ac0aed77a39c9
sunzu chris <sunzuchris250@gmail.com>
2025-01-19 08:17:27 +0200

    just commented uodate and delete account

--- app/api/accounts/[id]/route.ts
+++ app/api/accounts/[id]/route.ts
@@ -29,51 +29,51 @@ import { NextRequest, NextResponse } from "next/server";
     }
 }
 
- export async function PATCH(req: NextRequest,  { params }: { params: { id: string } }) {
-  try {
-    const id = parseInt(params.id, 10);
-      const data = await req.json();
+//  export async function PATCH(req: NextRequest,  { params }: { params: { id: string } }) {
+//   try {
+//     const id = parseInt(params.id, 10);
+//       const data = await req.json();
   
-      const updatedAccount = await updateAccount(id, data);
+//       const updatedAccount = await updateAccount(id, data);
   
-      if (!updatedAccount.length) {
-        return NextResponse.json({ message: "Account not found" }, { status: 404 });
-      }
+//       if (!updatedAccount.length) {
+//         return NextResponse.json({ message: "Account not found" }, { status: 404 });
+//       }
   
-      return NextResponse.json({ message: "Account updated successfully", updatedAccount });
-    } catch {
-      return NextResponse.json({ message: "Error Creating Account" }, { status: 500 });
-    }
-  }
+//       return NextResponse.json({ message: "Account updated successfully", updatedAccount });
+//     } catch {
+//       return NextResponse.json({ message: "Error Creating Account" }, { status: 500 });
+//     }
+//   }
 
-  export async function DELETE(req: Request,  { params }: { params: { id: string } }) {
-    try {
-      const id = parseInt(params.id, 10);
+//   export async function DELETE(req: Request,  { params }: { params: { id: string } }) {
+//     try {
+//       const id = parseInt(params.id, 10);
     
-        if (!id) {
-          return NextResponse.json(
-            { message: "Account ID is required for deletion." },
-            { status: 400 }
-          );
-        }
+//         if (!id) {
+//           return NextResponse.json(
+//             { message: "Account ID is required for deletion." },
+//             { status: 400 }
+//           );
+//         }
     
-        const deletedAccount = await deleteAccount(Number(id));
+//         const deletedAccount = await deleteAccount(Number(id));
     
-        if (!deletedAccount) {
-          return NextResponse.json(
-            { message: "Account not found." },
-            { status: 404 }
-          );
-        }
+//         if (!deletedAccount) {
+//           return NextResponse.json(
+//             { message: "Account not found." },
+//             { status: 404 }
+//           );
+//         }
     
-        return NextResponse.json(
-          { message: "Account deleted successfully" },
-          { status: 200 }
-        );
-      } catch (error) {
-        return NextResponse.json(
-          { message: "Error deleting Account", error },
-          { status: 500 }
-        );
-      }
-    }
+//         return NextResponse.json(
+//           { message: "Account deleted successfully" },
+//           { status: 200 }
+//         );
+//       } catch (error) {
+//         return NextResponse.json(
+//           { message: "Error deleting Account", error },
+//           { status: 500 }
+//         );
+//       }
+//     }
