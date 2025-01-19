eb94954c51119013d5d64ce2d768a9a254df67e5
sunzu chris <sunzuchris250@gmail.com>
2025-01-19 08:50:08 +0200

    updated transaction get

--- app/api/transactions/[id]/route.ts
+++ app/api/transactions/[id]/route.ts
@@ -1,18 +1,22 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 import { db } from '../../../../lib/db';
 import { transactions } from '../../../../db/schema';
 import { eq } from "drizzle-orm";
+// import {NextRequest} from 'next/server'
 
-import { 
-    updateTransaction,
-    deleteTransaction
- } from "../../../services/transactionService";
-import { PagesRouteHandlerContext } from 'next/dist/server/route-modules/pages/module.compiled';
+// import { 
+//     updateTransaction,
+//     deleteTransaction
+//  } from "../../../services/transactionService";
+// import { PagesRouteHandlerContext } from 'next/dist/server/route-modules/pages/module.compiled';
 
 
- export async function GET(req: Request,context: PagesRouteHandlerContext<{ id: string }>) {
-  try {
-      const accountId = parseInt(context.params.id, 10);
+ export async function GET(req: Request,context: { params: Promise<{ id: string }> }
+ ) {
+   try {
+        // Note the await here
+        const { id } = await context.params;
+        const accountId = parseInt(id, 10);
 
         if(isNaN(accountId)) {
             return NextResponse.json({ error: 'Invalid account ID format' }, { status: 400 });
@@ -32,52 +36,52 @@ import { PagesRouteHandlerContext } from 'next/dist/server/route-modules/pages/m
     }
 }
 
- export async function PATCH(req: NextRequest, context: PagesRouteHandlerContext<{ id: string }>) {
-     try {
-         const id = parseInt(context.params.id, 10);
-      const data = await req.json();
+//  export async function PATCH(req: NextRequest, context: PagesRouteHandlerContext<{ id: string }>) {
+//      try {
+//          const id = parseInt(context.params.id, 10);
+//       const data = await req.json();
   
-      const updatedTransaction = await updateTransaction(id, data);
+//       const updatedTransaction = await updateTransaction(id, data);
   
-      if (!updatedTransaction.length) {
-        return NextResponse.json({ message: "Transaction not found" }, { status: 404 });
-      }
+//       if (!updatedTransaction.length) {
+//         return NextResponse.json({ message: "Transaction not found" }, { status: 404 });
+//       }
   
-      return NextResponse.json({ message: "Transaction updated successfully", updatedTransaction });
-    } catch {
-      return NextResponse.json({ message: "Error Creating Transaction" }, { status: 500 });
-    }
-  }
+//       return NextResponse.json({ message: "Transaction updated successfully", updatedTransaction });
+//     } catch {
+//       return NextResponse.json({ message: "Error Creating Transaction" }, { status: 500 });
+//     }
+//   }
 
 
-export async function DELETE(req: Request, context: PagesRouteHandlerContext<{ id: string }>) {
-  try {
-      const id = parseInt(context.params.id, 10);
+// export async function DELETE(req: Request, context: PagesRouteHandlerContext<{ id: string }>) {
+//   try {
+//       const id = parseInt(context.params.id, 10);
   
-      if (!id) {
-        return NextResponse.json(
-          { message: "Transaction ID is required for deletion." },
-          { status: 400 }
-        );
-      }
+//       if (!id) {
+//         return NextResponse.json(
+//           { message: "Transaction ID is required for deletion." },
+//           { status: 400 }
+//         );
+//       }
   
-      const deletedTransaction = await deleteTransaction(Number(id));
+//       const deletedTransaction = await deleteTransaction(Number(id));
   
-      if (!deletedTransaction) {
-        return NextResponse.json(
-          { message: "Transaction not found." },
-          { status: 404 }
-        );
-      }
+//       if (!deletedTransaction) {
+//         return NextResponse.json(
+//           { message: "Transaction not found." },
+//           { status: 404 }
+//         );
+//       }
   
-      return NextResponse.json(
-        { message: "Transaction deleted successfully" },
-        { status: 200 }
-      );
-    } catch (error) {
-      return NextResponse.json(
-        { message: "Error deleting transaction", error },
-        { status: 500 }
-      );
-    }
-  }
+//       return NextResponse.json(
+//         { message: "Transaction deleted successfully" },
+//         { status: 200 }
+//       );
+//     } catch (error) {
+//       return NextResponse.json(
+//         { message: "Error deleting transaction", error },
+//         { status: 500 }
+//       );
+//     }
+//   }
